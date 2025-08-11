4f062b83bdb636029bb0f99b83f495ade6a94206
Vishwa <nvvishwa7805@gmail.com>
2025-08-11 14:17:40 +0530

    Updation on Login form UI

--- ecom-web/src/components/Login/Login.jsx
+++ ecom-web/src/components/Login/Login.jsx
@@ -1,20 +1,20 @@
 import login from './Login.module.css';
-import { FaGoogle } from "react-icons/fa";
+import googleLogo from '../../assets/google_logo.svg'
 
 const Login = ({setShowLogin}) => {
     return (
         <main className={login.loginOverlay} onClick={() => setShowLogin(false)}>
-            <section className={login.loginContainer}>
+            <section className={login.loginContainer} onClick={(e) => e.stopPropagation()}>
                 <div className={login.loginHeader}>
-                    <h3>Sign in to QuickCart</h3>
-                    <p>Welcome! Please sign in to continue</p>
+                    <h3>Sign in to QuickCart</h3>   
+                    <p style={{fontSize: '0.8em'}}>Welcome! Please sign in to continue</p>
                     <div className={login.socialLogin}>
-                        <FaGoogle />
+                        <img src={googleLogo} alt="google" style={{ width: '16px', height: '16px' }} />
                         <button className={login.googleButton}>Sign in with Google</button>
                     </div>
                 </div>
                 <div className={login.loginForm}>
-                    <p>Email address</p>
+                    <p style={{color: 'black'}}>Email address</p>
                     <input type="text" placeholder='Enter your email' />
                     <button>Sign in</button>
                 </div>
